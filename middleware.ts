ed96dbc49a5baa33950e1ca365966e8047bf2646
profitG <dldbsry55@gmail.com>
2024-07-18 20:58:09 +0900

    middleware

---
+++ middleware.ts
@@ -0,0 +1,32 @@
+import { NextRequest, NextResponse } from "next/server";
+import getSession from "./lib/session";
+import { redirect } from "next/navigation";
+
+interface Routes {
+  [key: string]: boolean;
+}
+
+const publicOnlyUrl: Routes = {
+  "/": true,
+  "/login": true,
+  "/sms": true,
+  "/create-account": true,
+};
+
+export async function middleware(request: NextRequest) {
+  const session = await getSession();
+  const exists = publicOnlyUrl[request.nextUrl.pathname];
+  if (!session.id) {
+    if (!exists) {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+  } else {
+    if (exists) {
+      return NextResponse.redirect(new URL("/profile", request.url));
+    }
+  }
+}
+
+export const config = {
+  matcher: ["/", "/profile", "/create-account"],
+};
